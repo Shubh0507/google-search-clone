aa3eb130dca38c47d8b1eb4b08045a1c6351d6c6
Shubh Bhaskar <shuhbbhaskar2005@gmail.com>
2025-04-19 02:33:30 +0530

    recommiting some missing changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useState } from 'react'
+// import {Footer} from "./components/footer"
+// import {Header} from "./components/header"
+// import {Routes} from "./components/routes"
 
 const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  function switchMode() {
+    setDarkMode((mode) => !mode);
+  }
+
   return (
-    <div>
-      Hello
+    <div className={darkMode ? 'dark' : ''}>
+      <div className='bg-grey-200 dark:bg-grey-700'>
+        Hello
+        <br/>
+        <button onClick={switchMode}>Dark Mode</button>
+      </div>
     </div>
   )
 }
